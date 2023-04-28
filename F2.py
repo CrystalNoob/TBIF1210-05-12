777614e015c5b198f1654393d80309af7e6312cd
CrystalNoob <69108822+CrystalNoob@users.noreply.github.com>
2023-04-28 22:34:04 +0700

    Done and Well commented

--- F2.py
+++
@@ -1,9 +0,0 @@
-# Prosedur Logout
-def logout():
-    listOfGlobals = globals()
-    if listOfGlobals['logged']:
-        listOfGlobals['logged'] = False
-        listOfGlobals['loggedUser'] = False
-        listOfGlobals['role'] = ''
-    else:
-        print("Logout gagal!\nAnda belum login, silahkan login terlebih dahulu sebelum melakukan logout")
